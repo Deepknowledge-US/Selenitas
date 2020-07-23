d14f16ba285dddbfbe0706b82beb1ecc989abe09
one <e.l.o.universidad@gmail.com>
2020-07-23 12:28:29 +0200

    Example_histogram added

--- Engine/config_file.lua
+++ Engine/config_file.lua
@@ -2,9 +2,9 @@ local config = {}
 
 config.ticks = 200 -- iteraciones
 
-config.xsize = 10  -- Dimensiones del mundo. De momento un plano.
+config.xsize = 60  -- Dimensiones del mundo. De momento un plano.
 
-config.ysize = 10
+config.ysize = 60
 
 config.setup = true -- De momento suponemos los botones setup y go como pulsados
 
