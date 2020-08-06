b060e7c59bdc84d0c73ed2c3cc51f5a07abe0218
Antonio Torres Moríñigo <atorresm@protonmail.com>
2020-08-06 17:05:20 +0200

    Allow running external files

--- main.lua
+++ main.lua
@@ -2,7 +2,5 @@ if not arg[2] then
     print("Usage: love . [path to source code file of the simulation]")
     love.event.quit()
 else
-    -- Run main file without the .lua extension
-    local path, _ = string.gsub(arg[2], ".lua", "")
-    require(path)
+    dofile(arg[2])
 end
