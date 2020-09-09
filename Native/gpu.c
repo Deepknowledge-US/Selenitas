59a03b7e419bc8ad7a027d7d4baa3e329d51a8fb
Antonio Torres Moríñigo <atorresm@protonmail.com>
2020-09-09 16:11:25 +0200

    Add reduce C function

--- Native/gpu.c
+++ Native/gpu.c
@@ -25,16 +25,31 @@ int sum_agent_x_coords(lua_State *l) {
     return 1;
 }
 
-// This is printed just after the application is closed
-int hello_world(lua_State *l) {
-    printf("Hello world!\n");
-    return 0;
+// Params: items list
+int reduce(lua_State *l) {
+    double ret = 0;
+    int t = lua_gettop(l);
+
+    lua_pushnil(l);
+    while (lua_next(l, t) != 0) {
+        // t[i] is on top of stack now
+        if (!lua_isnumber(l, lua_gettop(l))) {
+            lua_pushstring(l, "Reduce: all items on the table must be numbers");
+            lua_error(l);
+            lua_pop(l, 1); // pop error string
+        }
+        ret += lua_tonumber(l, -1);
+        lua_pop(l, 1); // pop iteration value
+    }
+
+    lua_pushnumber(l, ret);
+    return 1;
 }
 
 int luaopen_Native_gpu(lua_State *L) {
     luaL_Reg fns[] = {
         {"sum_agent_x_coords", sum_agent_x_coords},
-        {"hello_world", hello_world},
+        {"reduce", reduce},
         {NULL, NULL}
     };
     luaL_register(L, "gpu", fns); // function from Lua5.1
