dd9e5f744da55ec9c07d03acfa0b0e47b7e27ce0
Antonio Torres Moríñigo <atorresm@protonmail.com>
2020-09-18 14:22:55 +0200

    Add template file

---
+++ Resources/files/template.lua
@@ -0,0 +1,9 @@
+require 'Engine.utilities.utl_main'
+
+SETUP = function()
+    -- Write here the setup function for your simulation
+end
+
+RUN = function()
+    -- Write here the step function for your simulation
+end
