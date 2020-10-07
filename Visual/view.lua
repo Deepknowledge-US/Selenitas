5df1ef11ac5763e1ca3c32696048823a54e16dea
one <e.l.o.universidad@gmail.com>
2020-10-07 13:56:05 +0200

    updates in zoom and center information

--- Visual/view.lua
+++ Visual/view.lua
@@ -10,6 +10,10 @@ Input.add_mouse_moved_callback_func(
     function(x, y, dx, dy)
         if Input.is_mouse_button_pressed(2) then
             camera:translate(-dx / camera.scale, -dy / camera.scale)
+
+            local worldX, worldY = camera:getWorldCoordinates( x,y )
+            -- print(x,y)
+            Observer:set_center( { round(worldX - x, 3), round(worldY + y, 3) } )
         end
     end
 )
@@ -17,6 +21,8 @@ Input.add_scroll_callback_func(
     function(dx, dy)
         local inc = 1 + dy / 25
         camera:scaleToPoint(inc)
+
+        Observer:set_zoom( round(camera.scale,3) )
     end
 )
 
@@ -24,12 +30,17 @@ function View.init()
     local w, h, _ = love.window.getMode()
     camera = Camera(w, h, {translationX = w / 2, translationY = h / 2,
         resizable = true, maintainAspectRatio = true})
+
+    Observer:set_center( { camera.x, camera.y } )
 end
 
 function View.reset()
     local w, h, _ = love.window.getMode()
     camera:setTranslation(w / 2, h / 2)
     camera:setScale(1)
+
+    Observer:set_center( { camera.x, camera.y } )
+    Observer:set_zoom( camera.scale )
 end
 
 function View.update()
